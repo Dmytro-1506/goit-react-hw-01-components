6cdb58d4483507cb30743cd8f976ef123acba4bb
Dmytro-1506 <kyunlik@gmail.com>
2023-02-02 23:15:25 +0100

    Update Friends.js

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -16,8 +16,6 @@ const statusColor = (value) => {
 
 export const FriendList = ({ friends }) => {
     const newList = friends.map(item => {
-        console.log(item.isOnline);
-        console.log(statusColor(item.isOnline));
         return <li key={item.id} className="card">
             <span style={{backgroundColor: statusColor(item.isOnline)}} className="status"></span>
             <img className="avatar" src={item.avatar} alt="User avatar" width="100" height="100" />
